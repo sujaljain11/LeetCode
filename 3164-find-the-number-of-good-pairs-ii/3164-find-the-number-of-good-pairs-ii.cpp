16bf7c7f87d3d192d5853c54bca1c591200d16e6
Sujal Jain <104662536+sujaljain11@users.noreply.github.com>
2024-05-31 07:13:48 +0530

    Time: 1226 ms (58.23%), Space: 168.6 MB (70.30%) - LeetHub

--- 3164-find-the-number-of-good-pairs-ii/3164-find-the-number-of-good-pairs-ii.cpp
+++ 3164-find-the-number-of-good-pairs-ii/3164-find-the-number-of-good-pairs-ii.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     long long numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         unordered_map<long long int, long long int> hashmap;
-        long long ans = 0;
+        long long int ans = 0;
+        for(auto num : nums2) hashmap[num * k]++;
         for(auto num : nums1) {
             for(int i = 1; i <= sqrt(num); i++) {
                 if(num % i == 0) {
-                    hashmap[i]++;
-                    if(i != num / i) hashmap[num / i]++;
+                    if(hashmap.find(i) != hashmap.end()) ans += hashmap[i];
+                    
+                    if(i != num / i && hashmap.find(num / i) != hashmap.end()) ans += hashmap[num / i];
                 }
             }
-        }
-        for(auto num : nums2) {
-            ans += hashmap[num * k];
         }
         return ans;
     }
