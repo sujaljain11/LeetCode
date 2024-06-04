618cc7e5633d30456458160b16951a2bdbcdf89b
Sujal Jain <104662536+sujaljain11@users.noreply.github.com>
2024-06-04 09:39:07 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (48.94%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        map<char, int> hashmap;
-        for(int i = 0; i < s.length(); i++) {
-            hashmap[s[i]]++;
+        int oddFrequencyCount = 0;
+        unordered_map<char, int> frequencyMap;
+        for(auto character: s) {
+            frequencyMap[character]++;
+            if(frequencyMap[character] % 2 == 1) oddFrequencyCount ++;
+            else oddFrequencyCount--;
         }
-        int result = 0;
-        bool flag = false;
-        for(auto it: hashmap) {
-            if(it.second % 2 == 0) result += it.second;
-            else {
-                result += it.second - 1;
-                flag = true;
-            }
-        }
-        if(flag) return result + 1;
-        else return result;
+        if(oddFrequencyCount > 0) return s.length() - oddFrequencyCount + 1;
+        else return s.length();
     }
 };
